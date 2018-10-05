9827d9d4971c8d8db9797b497109d03faf340eb1
alaichkann <alaichkann@alaichkanndeMacBook-Pro.local>
2018-10-05 20:16:05 +0800

    watch button text 就只有这样啦，更改的结果就是这么回事！

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>你好呀！</h1>
-    <p>欢迎来到你的第一个pages～.</p>
-    <p>来创造一个新的世界吧！.</p>
-    <Link to="/page-2/">去第二页 ></Link>
+    <h1>你好呀！小伙子。😄</h1>
+    <p>来创造一个新的世界吧！是什么乎卡和可视电话季卡可接受的.</p>
+    <Link to="/page-2/">去 第二页吧 ></Link>
   </div>
 )
 
